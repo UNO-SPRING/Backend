568033d6bceadd64bb1170d247f4c6ebb8eecef3
isra2021 <egancarry@gmail.com>
2025-01-08 22:09:58 -0300

    Normalizar saltos de línea (LF -> CRLF)

---
+++ src/test/java/com/uno/UnoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
